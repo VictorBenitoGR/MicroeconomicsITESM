f81d077375a4eaddf9753fef2058752614b43e87
Victor Garcia <victorbenitogr@gmail.com>
2024-02-16 11:25:10 -0600

    Prettier plot

--- src/ppf_marginal_cost.R
+++ src/ppf_marginal_cost.R
@@ -1,5 +1,6 @@
 # * = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 # * MICROECONOMICS
+# * https://github.com/VictorBenitoGR/MicroeconomicsITESM
 # * = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 
 # * Imagine you are in the middle of nowhere and you can only hunt rabbits or
@@ -10,9 +11,11 @@
 # * Packages
 library(dplyr) # Data manipulation
 library(ggplot2) # Data visualization
-library(gganimate) # Animated plots with ggplot2
-library(babynames) # Baby names dataset
+library(gganimate) # // Animated plots with ggplot2
+library(babynames) # // Baby names dataset
 library(hrbrthemes) # Custom themes for ggplot2
+library(grid) # // Graphic system base
+library(png) # // PNG image handling
 
 # * Create the DF with the given info
 data <- data.frame(
@@ -33,29 +36,34 @@ ppf <- ggplot(data, aes(x = rabbits, y = blackberries)) +
 
 # * Obtain the opportunity cost of catching rabbits at
 # * each point on the graph (marginal cost).
-
-# Calculate the marginal cost at each point
 data$marginal_cost <- c(NA, diff(data$blackberries) / diff(data$rabbits))
 print(data)
-data$difference <- c(NA, diff(data$blackberries))
 
-# Plot | Production-Possibility Frontier (PPF) with marginal cost
+# * Plot | Production-Possibility Frontier (PPF) with marginal cost
 ppf_marginal_cost <- ggplot(data, aes(x = rabbits, y = blackberries)) +
+  geom_area(fill = "#69b3a2", alpha = 0.4) +
   geom_line(color = "#ff0000", size = 2, alpha = 0.9, linetype = 1) +
-  geom_text(aes(label = difference), vjust = 0, hjust = -2.5, size = 5) +
-  geom_point(shape = 19, color = "black", size = 5) + # , fill = ,
-  labs(
-    title = "Production-Possibility Frontier (PPF) with margin cost",
-    x = "Number of Rabbits",
-    y = "Number of Blackberries"
+  geom_text(aes(label = marginal_cost), vjust = 0.3, hjust = -1.5, size = 5) +
+  annotate(
+    "text",
+    label = "Inefficient",
+    x = 2, y = 100, size = 5, colour = "#000000"
   ) +
+  annotate(
+    "text",
+    label = "Impossible",
+    x = 4, y = 250, size = 5, colour = "#000000"
+  ) +
+  geom_point(shape = 19, color = "black", size = 5) + # , fill = ,
   theme_ipsum() +
   labs(
-    title = "Production-Possibility Frontier (PPF) with margin cost",
-    x = "Number of Rabbits", y = "Number of Blackberries"
-  ) +
-  theme(plot.title = element_text(face = "bold"))
+    title = "Production-Possibility Frontier (PPF) with margin cost | 🐇 Vs 🍇",
+    subtitle = "Imagine you are in the middle of nowhere and you can only hunt rabbits or pick blackberries to feed yourself.", # nolint: line_length_linter.
+    x = "Number of Rabbits", y = "Number of Blackberries",
+    caption = "R Plot: @VictorBenitoGR | GitHub Repository: VictorBenitoGR/MicroeconomicsITESM", # nolint: line_length_linter.
+  )
 
+# Save the plot
 ggsave("./assets/ppf_marginal_cost.jpg", ppf_marginal_cost,
   width = 8, height = 6
 )
